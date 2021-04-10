40ec8c8c664f7ba602cf9b8723c31941d8b74ec8
Jules Kulcsar <kulcsar_jules@yahoo.com>
2021-04-10 23:29:13 +0300

    get the smallest three numbers in array

---
+++ getMinThreeNumbers.js
@@ -0,0 +1,23 @@
+function min_three(arr) {
+    let one = +Infinity;
+    let two = +Infinity;
+    let three = +Infinity;
+
+    for (let i = 0; i < arr.length; i += 1) {
+        let num = arr[i];
+        if (num < three) {
+            if (num <= two) {
+                three = two;
+                if (num <= one) {
+                    two = one;
+                    one = num;
+                } else {
+                    two = num;
+                }
+            } else {
+                three = num;
+            }
+        }
+    }
+    return [one, two, three];
+}
